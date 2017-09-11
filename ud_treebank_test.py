26f0574f8bc54408e7d3420d029ec2c9ce19f2a6
Oscar Kalldal <oscar@kalldal.se>
2017-09-11 09:58:58 +0200

    finished poc EM, started debugging, does not run

--- ud_treebank_test.py
+++ ud_treebank_test.py
@@ -35,13 +35,14 @@ def compile_feature_lists(tag_keys, feature_keys, graphs, wanted_tags=None):
     feature_lists = dict()
     for dg in graphs:
         for node in dg.nodes:
-            features = extract_features(dg, node, tag_keys+feature_keys)
-            tag = '_'.join(features[:len(tag_keys)])
-            features = dict(zip(feature_keys, features[len(tag_keys):]))
-            if wanted_tags is None or tag in wanted_tags:
-                if tag not in feature_lists.keys():
-                    feature_lists[tag] = []
-                feature_lists[tag].append(features)
+            if node != 0:
+                features = extract_features(dg, dg.get_by_address(node), tag_keys+feature_keys)
+                tag = '_'.join(features[:len(tag_keys)])
+                features = dict(zip(feature_keys, features[len(tag_keys):]))
+                if wanted_tags is None or tag in wanted_tags:
+                    if tag not in feature_lists.keys():
+                        feature_lists[tag] = []
+                    feature_lists[tag].append(features)
     return feature_lists
 
 
@@ -59,16 +60,20 @@ class ParameterSet:
 
         feature_types = feature_vector_list[0].keys()
         max_feature_vals = 0
+        current_type_id = 0
         for feature_type in feature_types:
+            if feature_type not in self.feature_type_map.keys():
+                self.feature_type_map[feature_type] = current_id
+                current_type_id = current_type_id + 1
             self.feature_val_map[feature_type] = dict()
-            current_id = 0
+            current_val_id = 0
             for feature_vector in feature_vector_list:
                 feature_val = feature_vector[feature_type]
-                if feature_val not in self.feature_val_map[feature_type]:
-                    self.feature_val_map[feature_type][feature_val] = current_id
-                    current_id = current_id + 1
+                if feature_val not in self.feature_val_map[feature_type].keys():
+                    self.feature_val_map[feature_type][feature_val] = current_val_id
+                    current_val_id = current_val_id + 1
                 pass
-            max_feature_vals = max(max_feature_vals, current_id)
+            max_feature_vals = max(max_feature_vals, current_val_id)
 
         self.params = np.zeros(
             [len(sense_list), len(feature_types), max_feature_vals])
@@ -82,8 +87,11 @@ class ParameterSet:
 
 
 def estimate_sense_probabilities(feature_vector, parameter_set):
-    feature_parameters = np.stack([parameter_set.params[:, f_type, f_val]
-                                   for f_type, f_val in feature_vector.items()], axis=1)
+    feature_parameters = []
+    for f_type, f_val in feature_vector.items():
+        f_type_index = parameter_set.feature_type_map[f_type]
+        f_val_index = parameter_set.feature_val_map[f_type][f_val]
+        feature_parameters.append(parameter_set.params[:, f_type_index, f_val_index])
     likelihoods = np.prod(feature_parameters, axis=1)
     probabilities = likelihoods / sum(likelihoods)
     return probabilities
@@ -93,8 +101,10 @@ def update_parameter_set(feature_vector_list, sense_probabilities_list, paramete
     new_parameters = np.zeros(np.shape(parameter_set.params))
     for feature_vector, sense_probabilities in zip(feature_vector_list, sense_probabilities_list):
         for feature_type, feature_val in feature_vector.items():
-            feature_type_id = new_parameters.feature_type_map[feature_type]
-            feature_val_id = new_parameters.feature_val_map[feature_val]
+            feature_type_id = parameter_set.feature_type_map[feature_type]
+            feature_val_id = parameter_set.feature_val_map[feature_type][feature_val]
+            print(new_parameters[:, feature_type_id, feature_val_id].shape)
+            print(sense_probabilities.shape)
             new_parameters[:, feature_type_id, feature_val_id] =\
                 new_parameters[:, feature_type_id, feature_val_id] + sense_probabilities
     parameter_set.params = new_parameters / np.sum(new_parameters, axis=2)
@@ -104,8 +114,35 @@ def randomize_parameter_set(parameter_set):
         feature_type_id = parameter_set.feature_type_map[feature_type]
         feature_val_length = len(parameter_set.feature_val_map[feature_type])
         sense_length = len(parameter_set.sense_map)
-        random_probabilities = np.random.rand([sense_length, feature_val_length])
-        random_probabilities = random_probabilities / sum(random_probabilities, axis=1)
+        random_probabilities = np.random.rand(sense_length, feature_val_length)
+        random_probabilities = random_probabilities / np.sum(random_probabilities, axis=1)
         parameter_set.params[:, feature_type_id, :feature_val_length] = random_probabilities
+
+def estimate_parameters_EM(file_path, tag_keys, feature_keys, tags_to_senses, iterations=10):
+    graphs = parse_connlu_file(file_path)
+    wanted_tags = tags_to_senses.keys()
+    feature_vector_lists = compile_feature_lists(tag_keys, feature_keys, graphs, wanted_tags)
+
+    params_dict = dict()
+    for tag in wanted_tags:
+
+        parameter_set = ParameterSet(feature_vector_lists[tag], tags_to_senses[tag])
+        randomize_parameter_set(parameter_set)
+        feature_vector_list = feature_vector_lists[tag]
+        for i in range(iterations):
+            sense_probabilities_list = []
+            for feature_vector in feature_vector_list:
+                sense_probabilities = estimate_sense_probabilities(feature_vector, parameter_set)
+                sense_probabilities_list.append(sense_probabilities)
+            update_parameter_set(feature_vector_list, sense_probabilities_list, parameter_set)
+        params_dict[tag] = parameter_set
+
+UD_FILE = 'en-ud-dev.conllu.txt'
+tag_keys = ['lemma', 'ctag']
+feature_keys = ['rel', 'head_lemma', 'head_ctag']
+tags_to_senses = {'come_VERB' : ['come_1', ]}
+estimated_params = estimate_parameters_EM(UD_FILE, tag_keys, feature_keys, tags_to_senses)
+
+
 
 
