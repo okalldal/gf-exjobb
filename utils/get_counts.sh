13aef7ef9e6c4a6444b288a9d07284e2cdcf14ae
Maximilian Ludvigsson <maxiludvigsson@gmail.com>
2017-10-11 10:54:48 +0200

    Fixes the get_counts bashscript

--- utils/get_counts.sh
+++ utils/get_counts.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-TARBALL = "https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-1989/Swedish-annotated-conll17.tar?sequence=41&isAllowed=y"
+TARBALL="https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-1989/Swedish-annotated-conll17.tar?sequence=41&isAllowed=y"
+LANGUAGE='swe'
+#SORT_OPTS="-S 50% --parallel=8 -T $PWD/tmp";
+SORT_OPTS="-S 50% -T $PWD/tmp"
+MAX_LINES=1000000
+
+
+mkdir -p $PWD/tmp
 
 # Discover filenames
 # curl -sL $TARBALL | tar -tv | head -n 20
 
-FILENAME = 
-# curl -sL $TARBALL | tar -xO $FILENAME | unxz | python conllu_parser.py | head -n 1000000 > tmp/buffer.tmp
-
-# cat tmp/buffer.tmp | sort -k1 | uniq -c | awk '{print $1 "\t" $2"\t"$3}'
+# Download the top NUM conllu-lines and save to a temp file
+BUFFER=tmp/buffer-$LANGUAGE.tmp
+curl -sL $TARBALL | tar -xO | unxz | python conllu_parser.py | head -n $MAX_LINES > $BUFFER
+echo "Downloaded UD sentences, starting to count"
+# read the file and do the counts
+cat $BUFFER | cut -f1,5 | sort $SORT_OPTS -k1 | uniq -c | awk '{print $1 "\t" $2"\t"$3}' > $LANGUAGE.counts
+rm $BUFFER 
+echo "Wrote file $LANGUAGE.counts"
