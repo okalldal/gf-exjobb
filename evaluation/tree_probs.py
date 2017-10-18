dd3a533b0e3097daf0c4f2e999b35b0a8d05fb83
Maximilian Ludvigsson <maxiludvigsson@gmail.com>
2017-10-18 16:29:36 +0200

    update tree_probs to use new probfiles

--- evaluation/tree_probs.py
+++ evaluation/tree_probs.py
@@ -37,26 +37,27 @@ def find_heads(expression, prev_heads = [], label='root'):
         return out, head
 
 
-def tree_prob(tree_tuples, probs):
+def tree_prob(tree_tuples, bigramprobs, unigramprobs):
     
     total = 0
-    marginal_head = Memoize(lambda fun: sum(p for (a, b), p in probs.items() if b == fun))
-    marginal_child = Memoize(lambda fun: sum(p for (a, b), p in probs.items() if a == fun))
 
     bigram_count = 0
     unigram_count = 0
     for node, heads in tree_tuples:
-        head = heads[0] if len(heads) > 0 else 'root'
-        bigram_prob = probs[(node, head)]
-        unigram_prob = marginal_child(node)
+        head = heads[0] if len(heads) > 0 else 'ROOT'
+        bigram_prob = bigramprobs[(node, head)]
+        unigram_prob = unigramprobs[node]
 
         if bigram_prob != 0:
+            print("({},{}): {}".format(node, head, bigram_prob))
             bigram_count += 1
-            prob = log(bigram_prob) - log(marginal_head(head))
+            prob = log(bigram_prob) - log(unigramprobs[head])
         elif unigram_prob != 0:
+            print("({},): {}".format(node, unigram_prob))
             unigram_count += 1
             prob = log(unigram_prob)
         else:
+            print("{} no collected data".format(node))
             prob = 0
 
         total = total-prob
@@ -65,27 +66,40 @@ def tree_prob(tree_tuples, probs):
     logging.info(msg % (bigram_count, unigram_count))
     return total
 
+def run(sentence, grammar, bigramprobs, unigramprobs):
+    eng = grammar.languages['TranslateEng']
+    swe = grammar.languages['TranslateSwe']
+    
+    print(sentence)
+    print('GF\t\tUs\t\tTotal\t\tTranslation')
+    for i, (p, ex) in enumerate(eng.parse(sentence)):
+        tuples, _ = find_heads(ex)
+        bigrams = [(n, hs) for n, hs, l in tuples]
+        rerank_total = tree_prob(bigrams, bigramprobs, unigramprobs)
+        print(str(p) + '\t\t' +  str(rerank_total) + '\t\t' + str(p+rerank_total) +'\t\t' + swe.linearize(ex))
+        if i > 10:
+            break
+
+
+def init():
+    # unigram
+    with open('../results/prasanth_counts_total_unigram.probs') as f:
+        data = (l.strip().split('\t') for l in f)
+        unigramprobs = defaultdict(lambda: 0, ((t, float(p)) for (t, p) in data))
+    # bigram
+    # filter out non bigram probabilities, sometimes we get these
+    bigramprobs = defaultdict(lambda: 0, ((t, p) for (t, p) in read_probs('../results/prasanth_counts_total.probs') 
+                                           if len(t) == 2 ))
+    # GF
+    grammar = pgf.readPGF('../data/TranslateEngSwe.pgf')
+    return grammar, bigramprobs, unigramprobs
+
 
 if __name__ == "__main__":
 
     logging.basicConfig(level=logging.INFO)
-    
-    en_probs = defaultdict(lambda: 0, read_probs('../results/bigram_Eng.probs'))
-    total_probs = defaultdict(lambda: 0, read_probs('../results/bigram_total.probs'))
 
-    gr = pgf.readPGF('../data/TranslateEngSwe.pgf')
-    eng = gr.languages['TranslateEng']
-    swe = gr.languages['TranslateSwe']
-    
     sentence = sys.argv[1] if len(sys.argv) == 2 else \
-        "the horse likes to eat the hay which we all had selected" 
-    print(sentence)
-    print('GF\t\tEnglish\t\tTotal')
-    for i, (p, ex) in enumerate(eng.parse(sentence)):
-        tuples, _ = find_heads(ex)
-        bigrams = [(n, hs) for n, hs, l in tuples]
-        rerank_en = tree_prob(bigrams, en_probs)
-        rerank_total = tree_prob(bigrams, total_probs)
-        print(str(p) + '\t\t' + str(rerank_en) + '\t\t' + str(rerank_total))
-        if i > 20:
-            break
+        "he works at the bank" 
+    # bigramprobs, unigramprobs = probs()
+    # run(sentence, gr, bigramprobs, unigramprobs)
