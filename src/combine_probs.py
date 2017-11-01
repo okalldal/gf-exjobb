5d40756aff2411cf3efdc06da5cb8f1defd58a0d
Oscar Kalldal <oscar@kalldal.se>
2017-11-01 11:26:19 +0100

    added possibility to merge in specified ways in merge_probs

--- src/combine_probs.py
+++ src/combine_probs.py
@@ -1,14 +1,23 @@
 import argparse
+import gzip
 PYTHONIOENCODING="UTF-8"
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('directory', type=str, help='Probability directory')
+parser.add_argument('-s', type=str, default="", help='keep splits, separated by comma')
+parser.add_argument('-p', type=str, default='./', help='Probability directory')
+parser.add_argument('-o', type=str, default='./', help='Out directory')
 args = parser.parse_args()
-directory = args.directory
+in_directory = args.p
+out_directory = args.o
 total_count = 0
+#if parser.s == "":
+#    keep_splits = None
+#else
+keep_splits = [int(i) for i in args.s.split(',')]
 split_counts = list()
-with open(directory+'total_splits.txt', mode='r',encoding='utf-8') as file:
+
+with open(in_directory+'1_splits.txt', mode='r',encoding='utf-8') as file:
     for l in file:
         l_split = l.strip('\n').split('\t')
         count = int(l_split[0])
@@ -16,9 +25,15 @@ with open(directory+'total_splits.txt', mode='r',encoding='utf-8') as file:
         split_counts.append((split,count))
         total_count = total_count + count
 for split, count in split_counts:
-    with open(directory+split+'.txt', mode='r',encoding='utf-8') as file:
-        for l in file:
-            l_split = l.strip('\n').split('\t')
-            prob = float(l_split[1])
-            word = l_split[0]
-            print(word, prob*count/total_count, sep='\t')
+    with open(in_directory+split+'.probs', mode='r',encoding='utf-8') as file:
+        if keep_splits == []:
+            name = 'all'
+        else:
+            split_cols = split.split('_')
+            name = '_'.join([split_cols[i] for i in keep_splits])
+        with open(out_directory+name+'.probs', mode='a+') as out_file:
+            for l in file:
+                l_split = l.strip('\n').split('\t')
+                prob = float(l_split[0])
+                word = l_split[1:]
+                print(*([prob*count/total_count] + word), sep='\t', file=out_file)
