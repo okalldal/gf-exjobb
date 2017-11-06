f2c0de3ba4e15c02240c9462c031caa6f6778b4a
Oscar Kalldal <oscar@kalldal.se>
2017-11-06 13:53:47 +0100

    added unigrams recounting and combining probs to pipeline

--- data/possibility_dictionaries/combine_gf_wn.sh
+++ data/possibility_dictionaries/combine_gf_wn.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-for file in $(comm -12 <(ls gf) <(ls wn2))
+for file in $(comm -12 <(ls gf) <(ls wn))
 do
     cat wn/$file <(awk -v FS='\t' -v OFS='\t' '$2 !~ /NOUN|VERB|ADJ|ADV|PROPN/' gf/$file) |sort -k2,2 -k1,1> gf_wn/$file
 done
--- src/run_em.sh
+++ src/run_em.sh
@@ -4,7 +4,7 @@
 #WN langs Bul Chi Eng Fin Fre Ita Spa Swe
 #GF langs Bul Chi Dut Eng Fin Fre Ger Hin Ita Spa Swe
 #Test langs Bul Eng Fin Fre Swe
-PYTHONENIOCODING="UTF-8"
+PYTHONIOENCODING="UTF-8"
 unigram_opts="-f2 -p1"
 run_em () {
 if [ ! -d $2 ]; then
@@ -23,31 +23,63 @@ cut -f2- -d $'\t' > $2/${split}.probs
 done
 fi
 }
-echo "GF UD Gold"
-run_em ../data/em_data/gf_udgold ../results/gf_udgold
-echo "WN UD Gold"
-run_em ../data/em_data/wn_udgold ../results/wn_udgold
+combine_probs () {
+if [ ! -f $2 ]; then
+python combine_probs.py $1 > $2 
+fi
+}
+recount_no_deprel () {
+if [ ! -f $2 ]; then
+awk -v FS='\t' 'OFS="\t" {if (NF==4) {print $1, $2, $3} else {print $1,$2}}' $1 | sort -T tmp_sort_file -k2 | python merge_counts.py -f > $2
+fi
+}
+
+#echo "GF UD Gold"
+#run_em ../data/em_data/gf_udgold ../results/gf_udgold
+#echo "WN UD Gold"
+#run_em ../data/em_data/wn_udgold ../results/wn_udgold
 echo "GF Parsed th50"
 run_em ../data/em_data/gf_autoparsed_th50 ../results/gf_autoparsed_th50
-echo "GF Parsed th10"
-run_em ../data/em_data/gf_autoparsed_th10 ../results/gf_autoparsed_th10
-
-echo "Noeng GF Parsed th50"
-mkdir ../data/em_data/no_eng_gf_parsed
-for lang in ../data/em_data/gf_autoparsed_th50
-do
-ln -s $lang ../data/em_data/no_eng_gf_parsed/$(basename $lang)
-done
-rm ../data/em_data/no_eng_gf_parsed/eng
-run_em ../data/em_data/no_eng_gf_parsed ../results/no_eng_gf_parsed
-
-echo "Only eng GF Parsed th50"
-mkdir ../data/em_data/only_eng_gf_parsed
-ln -s ../results/gf_autoparsed_th50/eng ../data/em_data/only_eng_gf_parsed/eng
-run_em ../data/em_data/only_eng_gf_parsed ../results/only_eng_gf_parsed
-
-echo "Unigram GF UD-GOLD"
-run_em ../data/em_data/gf_uni_udgold ../results/gf_uni_udgold "${unigram_opts}"
+combine_probs ../results/gf_autoparsed_th50/ ../results/gf_autoparsed_th50.probs
+recount_no_deprel ../results/gf_autoparsed_th50.probs ../results/gf_nodep_autoparsed_th50.probs
 
 echo "Unigram GF Parsed th50"
 run_em ../data/em_data/gf_uni_autoparsed_th50 ../results/gf_uni_autoparsed_th50 "${unigram_opts}"
+combine_probs ../results/gf_uni_autoparsed_th50/ ../results/gf_uni_autoparsed_th50.probs
+recount_no_deprel ../results/gf_uni_autoparsed_th50.probs ../results/gf_uni_nodep_autoparsed_th50.probs
+
+echo "Unigram WN Parsed th50"
+run_em ../data/em_data/wn_uni_autoparsed_th50 ../results/wn_uni_autoparsed_th50 "${unigram_opts}"
+combine_probs ../results/wn_uni_autoparsed_th50/ ../results/wn_uni_autoparsed_th50.probs
+recount_no_deprel ../results/wn_uni_autoparsed_th50.probs ../results/wn_uni_nodep_autoparsed_th50.probs
+
+
+echo "WN Parsed th50"
+run_em ../data/em_data/wn_autoparsed_th50 ../results/wn_autoparsed_th50
+combine_probs ../results/wn_autoparsed_th50/ ../results/wn_autoparsed_th50.probs
+recount_no_deprel ../results/wn_autoparsed_th50.probs ../results/wn_nodep_autoparsed_th50.probs
+
+
+
+
+echo "Noeng WN Parsed th50"
+#mkdir ../data/em_data/no_eng_wn_parsed
+#for lang in ../data/em_data/wn_autoparsed_th50/*
+#do
+#ln -s $lang ../data/em_data/no_eng_wn_parsed/$(basename $lang)
+#done
+#rm ../data/em_data/no_eng_wn_parsed/eng
+#run_em ../data/em_data/no_eng_wn_parsed ../results/no_eng_wn_parsed
+
+echo "Only eng WN Parsed th50"
+#mkdir ../data/em_data/only_eng_wn_parsed
+#ln -s ../data/wn_autoparsed_th50/eng ../data/em_data/only_eng_wn_parsed/eng
+#run_em ../data/em_data/only_eng_wn_parsed ../results/only_eng_wn_parsed
+
+echo "Unigram GF UD-GOLD"
+#run_em ../data/em_data/gf_uni_udgold ../results/gf_uni_udgold "${unigram_opts}"
+
+echo "GF Parsed th10"
+#run_em ../data/em_data/gf_autoparsed_th10 ../results/gf_autoparsed_th10
+echo "WN Parsed th10"
+#run_em ../data/em_data/wn_autoparsed_th10 ../results/wn_autoparsed_th10
